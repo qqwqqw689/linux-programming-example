a21551fddb0b2e04bd384711af06424929d70ad5
qqwqqw689 <114795525+qqwqqw689@users.noreply.github.com>
2023-04-20 09:10:25 +0800

    updates

---
+++ wait.c
@@ -0,0 +1,70 @@
+#include <sys/wait.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+int
+main(int argc, char *argv[])
+{
+    pid_t cpid, w;
+    int wstatus;
+    cpid = fork();
+    // On success, the PID of the child process is returned in the parent, and 0 is returned in the child.
+    if (cpid == -1) {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    }
+    if (cpid == 0) {            /* Code executed by child */
+        printf("Child PID is %jd\n", (intmax_t) getpid());
+        // getpid() returns the process ID (PID) of the calling process.
+        // intmax_t 	maximum width integer type 
+        if (argc == 1)
+            pause();
+            // pause() causes the calling process (or thread) to sleep until a
+            // signal is delivered that either terminates the process or causes
+            // the invocation of a signal-catching function.
+        _exit(atoi(argv[1]));
+        // _exit() terminates the calling process "immediately". 
+        // int atoi (const char * str);
+        // Convert string to integer
+    } else {                    /* Code executed by parent */
+        do {
+            w = waitpid(cpid, &wstatus, WUNTRACED | WCONTINUED);
+            // pid_t waitpid(pid_t pid, int *stat_loc, int options);
+            // wait for a child process to stop or terminate
+            // pid_t : process ID or a process group ID to identify the child process or processes on which waitpid() should operate.
+            // stat_loc : Pointer to an area where status information about how the child process ended is to be placed.
+            // options : An integer field containing flags that define how waitpid() should operate.
+            // WUNTRACED : return if a child has stopped
+            // WCONTINUED : return if a stopped child has been resumed by delivery of SIGCONT
+            if (w == -1) {
+                perror("waitpid");
+                exit(EXIT_FAILURE);
+            }
+            if (WIFEXITED(wstatus)) {
+                printf("exited, status=%d\n", WEXITSTATUS(wstatus));
+            } else if (WIFSIGNALED(wstatus)) {
+                printf("killed by signal %d\n", WTERMSIG(wstatus));
+            } else if (WIFSTOPPED(wstatus)) {
+                printf("stopped by signal %d\n", WSTOPSIG(wstatus));
+            } else if (WIFCONTINUED(wstatus)) {
+                printf("continued\n");
+            }
+        } while (!WIFEXITED(wstatus) && !WIFSIGNALED(wstatus));
+        exit(EXIT_SUCCESS);
+    }
+}
+
+/*
+ $ ./a.out &
+           Child PID is 32360
+           [1] 32359
+           $ kill -STOP 32360
+           stopped by signal 19
+           $ kill -CONT 32360
+           continued
+           $ kill -TERM 32360
+           killed by signal 15
+           [1]+  Done                    ./a.out
+           $
+*/
