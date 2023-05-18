87be8649bbbfddef864fedae49bd923d673216a4
qqwqqw689 <114795525+qqwqqw689@users.noreply.github.com>
2023-05-18 11:41:54 +0800

    updates

--- pthread_cancel.c
+++ pthread_cancel.c
@@ -10,35 +10,41 @@ int  thstatus;
                                                                                 
 void * thread(void *arg)                                                        
 {                                                                               
- puts("thread has started. now sleeping");                                      
- while (1)                                                                      
-   sleep(1);                                                                    
+  puts("thread has started. now sleeping");                                      
+  while (1)                                                                      
+    sleep(1);
+    // unsigned int sleep(unsigned int seconds); - sleep for a specified number of seconds                                                                  
 }                                                                               
                                                                                 
-main(int argc, char *argv[])                                                    
+int main(int argc, char *argv[])                                                    
 {                                                                               
- pthread_t      thid;                                                           
- void           *status;                                                        
-                                                                                
- if ( pthread_create(&thid, NULL, thread, NULL) != 0) {                         
-   perror("pthread_create failed");                                             
-   exit(2);                                                                     
- }                                                                              
-                                                                                
- if ( pthread_cancel(thid) == -1 ) {                                            
-   perror("pthread_cancel failed");                                             
-   exit(3);                                                                     
- }                                                                              
-                                                                                
- if ( pthread_join(thid, &status)== -1 ) {                                      
-   perror("pthread_join failed");                                               
-   exit(4);                                                                     
- }                                                                              
-                                                                                
- if ( status == (int *)-1 )                                                     
-   puts("thread was cancelled");                                                
- else                                                                           
-   puts("thread was not cancelled");                                            
-                                                                                
- exit(0);                                                                       
+  pthread_t      thid;                                                           
+  void           *status;                                                        
+                                                                                 
+  if ( pthread_create(&thid, NULL, thread, NULL) != 0) {                         
+    perror("pthread_create failed");
+    // perror - print a system error message                                    
+    exit(2);                                                                     
+  }                                                                              
+                                                                                 
+  if ( pthread_cancel(thid) == -1 ) {                                            
+    perror("pthread_cancel failed");                                             
+    exit(3);                                                                     
+  }                                                                              
+                                                                                 
+  if ( pthread_join(thid, &status)== -1 ) { 
+    // int pthread_join(pthread_t thread, void **retval);
+    // The pthread_join() function waits for the thread specified by thread to terminate.
+    // If retval is not NULL, then pthread_join() copies the exit status of the target thread
+    // into the location pointed to by retval.                                    
+    perror("pthread_join failed");                                               
+    exit(4);                                                                     
+  }                                                                              
+                                                                                 
+  if ( status == (int *)-1 )                                                     
+    puts("thread was cancelled");                                                
+  else                                                                           
+    puts("thread was not cancelled");                                            
+                                                                                 
+  exit(0);                                                                       
 } 
