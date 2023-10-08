1a02d85a0c88524c0cd78b09622bf26679df799a
qqwqqw689 <114795525+qqwqqw689@users.noreply.github.com>
2023-10-08 19:42:14 +0800

    updates

--- stat.c
+++ stat.c
@@ -5,8 +5,7 @@
 #include <sys/sysmacros.h>
 #include <time.h>
 
-int
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
     struct stat sb;
 
@@ -37,22 +36,17 @@ main(int argc, char *argv[])
     default:       printf("unknown?\n");                break;
     }
 
+    // uintmax_t: unsigned integer type with the maximum width supported.
     printf("I-node number:            %ju\n", (uintmax_t) sb.st_ino);
-
-    printf("Mode:                     %jo (octal)\n",
-           (uintmax_t) sb.st_mode);
-
+    printf("Mode:                     %jo (octal)\n", (uintmax_t) sb.st_mode);
     printf("Link count:               %ju\n", (uintmax_t) sb.st_nlink);
-    printf("Ownership:                UID=%ju   GID=%ju\n",
-           (uintmax_t) sb.st_uid, (uintmax_t) sb.st_gid);
-
-    printf("Preferred I/O block size: %jd bytes\n",
-           (intmax_t) sb.st_blksize);
-    printf("File size:                %jd bytes\n",
-           (intmax_t) sb.st_size);
-    printf("Blocks allocated:         %jd\n",
-                  (intmax_t) sb.st_blocks);
-
+    printf("Ownership:                UID=%ju   GID=%ju\n", (uintmax_t) sb.st_uid, (uintmax_t) sb.st_gid);
+    printf("Preferred I/O block size: %jd bytes\n", (intmax_t) sb.st_blksize);
+    printf("File size:                %jd bytes\n", (intmax_t) sb.st_size);
+    printf("Blocks allocated:         %jd\n", (intmax_t) sb.st_blocks);
+    
+    // char* ctime (const time_t * timer);
+    // Convert time_t value to string
     printf("Last status change:       %s", ctime(&sb.st_ctime));
     printf("Last file access:         %s", ctime(&sb.st_atime));
     printf("Last file modification:   %s", ctime(&sb.st_mtime));
